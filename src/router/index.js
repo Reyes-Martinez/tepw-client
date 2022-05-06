d91ef0a0dea8f1c7e68c0e7ddc3ce2cc63d9c783
Edgar <edgar59156@gmail.com>
2022-05-06 16:27:32 -0500

    Agregado admin/category

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import AddCategory from '../views/Category/AddCategory.vue'
+import Category from "../views/Category/Category";
 const routes = [
   {
     path: '/',
@@ -14,7 +15,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path:'/admin/category/add',
+    name:'AddCategory',
+    component: AddCategory
+  },
+  {
+    path: "/admin/category",
+    name: "AdminCategory",
+    component: Category,
+  },
 ]
 
 const router = createRouter({
